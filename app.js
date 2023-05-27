ae5913b004dd09c66fa94f1c1f355d5d6f94d6ee
NadiaTurko <47818620+NadiaTurko@users.noreply.github.com>
2023-05-27 16:44:05 +0300

    Update app.js

--- app.js
+++ app.js
@@ -11,8 +11,15 @@ const checkArraysEqual = function (listOne, listTwo) {
   const checkElement = listOne.every(function (element, index) {
     return element === listTwo[index];
   });
-  return checkElement;
+
+  const checkLength = listOne.length === listTwo.length;
+  if (checkElement === true && checkLength === true) {
+    return true;
+  } else {
+    return false;
+  }
 };
 console.log(checkArraysEqual([], [])); // should return true
 console.log(checkArraysEqual([1], [])); // should return false
-console.log(checkArraysEqual([1, 5, 4], [1, 4, 5])); // should return true
+console.log(checkArraysEqual([], [1])); // should return false
+console.log(checkArraysEqual([1, 4, 5], [1, 4, 5])); // should return true
